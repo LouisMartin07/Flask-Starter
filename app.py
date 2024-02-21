069040008286248959bc3df1a98a5b831cc0d317
LouisMartin07 <louis.martin11730@gmail.com>
2024-02-21 10:31:47 -0600

    updated app routes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/login")
+@app.route("/")
 def login():
     return render_template("login.html")
 
@@ -11,19 +11,21 @@ def signup():
     return render_template("signup.html")
 
 @app.route("/recipes")
-def signup():
+def recipes():
     return render_template("recipes.html")
 
 @app.route("/mealplanning")
-def signup():
+def mealplanning():
     return render_template("mealplanning.html")
 
 @app.route("/grocerylistgen")
-def signup():
+def grocerylistgen():
     return render_template("grocerylistgen.html")
 
 @app.route("/custandpref")
-def signup():
+def custandpref():
     return render_template("custandpref.html")
 
 app.run(debug=True)
+
+
